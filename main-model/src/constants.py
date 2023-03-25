7d722e33c80af843d07e4d2b7d63a10f8cbba836
Your Name <you@example.com>
2023-03-25 23:30:10 +0700

    hotel recommendation

--- main-model/src/constants.py
+++ main-model/src/constants.py
@@ -7,3 +7,8 @@ RATING_ATTR_FILENAME = 'attraction_reviews.json'
 
 DIR_IMAGE = '../assets/images/'
 DIR_NO_IMAGE = '../assets/images/noimage.jpg'
+
+HOTEL_REC_APP_NAME = 'hotel_rec'
+DIR_DATA_HOTEL = '../data/hotel/hotels.json'
+DIR_DATA_HOTEL_AMENITIES = '../data/hotel/hotels_amenities.json'
+DIR_DATA_HOTEL_REVIEWS = '../data/hotel/user_review.json'
